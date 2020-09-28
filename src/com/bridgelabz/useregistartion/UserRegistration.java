b503d8084a66d5d01a25c1251d8a68764d22c266
Kauk777 <curokky777@gmail.com>
2020-09-28 18:50:23 +0530

    [KAUSHAL]Added: UC3 email validation

--- src/com/bridgelabz/useregistartion/UserRegistration.java
+++ src/com/bridgelabz/useregistartion/UserRegistration.java
@@ -6,6 +6,11 @@ import java.util.regex.Pattern;
 public class UserRegistration {
 	private static final Pattern FIRST_NAME=Pattern.compile("^[A-Z]{1}[A-Za-z]{2,}$");
 	private static final Pattern LAST_NAME=Pattern.compile("^[A-Z]{1}[A-Za-z]{2,}$");
+    private static final Pattern EMAIL_PATTERN=Pattern.compile("^[0-9a-zA-Z_+-]+(\\.?[0-9a-zA-Z_+-]*)[^\\.]@[0-9a-zA-Z]+(\\.[0-9a-zA-Z]{2,})(\\.?[a-zA-Z]{2,})?$");
+	
+	public boolean emailValidation(String email ) {
+		return EMAIL_PATTERN.matcher(email).matches();
+	}
 	
 	public boolean firstNameValidation(String fname) {
 		return FIRST_NAME.matcher(fname).matches();
@@ -22,8 +27,11 @@ public class UserRegistration {
 		String fname=sc.next();
 		System.out.println("Enter user last name: ");
 		String lname=sc.next();
+		System.out.println("Enter user email id: ");
+		String email=sc.next();
 		System.out.println("First Name validation: "+user.firstNameValidation(fname));
-		System.out.println("Second Name validation: "+user.firstNameValidation(lname));
+		System.out.println("Last Name validation: "+user.lastNameValidation(lname));
+		System.out.println("Email validation: "+user.emailValidation(email));
 		
 	}
 
