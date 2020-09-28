a4df89fc32a0c6643c18f627451334915999e97a
Kauk777 <curokky777@gmail.com>
2020-09-28 20:04:40 +0530

    [KAUSHAL]Added: UC7 atleast one digit

--- src/com/bridgelabz/useregistartion/UserRegistration.java
+++ src/com/bridgelabz/useregistartion/UserRegistration.java
@@ -8,7 +8,7 @@ public class UserRegistration {
 	private static final Pattern LAST_NAME=Pattern.compile("^[A-Z]{1}[A-Za-z]{2,}$");
     private static final Pattern EMAIL_PATTERN=Pattern.compile("^[0-9a-zA-Z_+-]+(\\.?[0-9a-zA-Z_+-]*)[^\\.]@[0-9a-zA-Z]+(\\.[0-9a-zA-Z]{2,})(\\.?[a-zA-Z]{2,})?$");
 	private static final Pattern MOBILE_PATTERN=Pattern.compile("^[0-9]{2}\\s{1}[6-9]{1}[0-9]{9}$");
-	private static final Pattern PASSWORD_PATTERN=Pattern.compile("^(?=.*[A-Z])([0-9a-z]).{8,}$");
+	private static final Pattern PASSWORD_PATTERN=Pattern.compile("^(?=.*[A-Z])(?=.*[0-9])([a-z])*.{8,}$");
 	
 	public boolean emailValidation(String email ) {
 		return EMAIL_PATTERN.matcher(email).matches();
