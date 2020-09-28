c95382a4da47bdeb138a3fe7d26968142dfe53cd
Kauk777 <curokky777@gmail.com>
2020-09-28 18:40:52 +0530

    [KAUSHAL]Added: UC2 last name validation

--- src/com/bridgelabz/useregistartion/UserRegistration.java
+++ src/com/bridgelabz/useregistartion/UserRegistration.java
@@ -5,9 +5,14 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 	private static final Pattern FIRST_NAME=Pattern.compile("^[A-Z]{1}[A-Za-z]{2,}$");
+	private static final Pattern LAST_NAME=Pattern.compile("^[A-Z]{1}[A-Za-z]{2,}$");
 	
 	public boolean firstNameValidation(String fname) {
 		return FIRST_NAME.matcher(fname).matches();
+	}
+	
+	public boolean lastNameValidation(String lname) {
+		return LAST_NAME.matcher(lname).matches();
 	}
 	
 	public static void main(String[] args) {
@@ -15,7 +20,10 @@ public class UserRegistration {
 		UserRegistration user=new UserRegistration();
 		System.out.println("Enter user first name: ");
 		String fname=sc.next();
+		System.out.println("Enter user last name: ");
+		String lname=sc.next();
 		System.out.println("First Name validation: "+user.firstNameValidation(fname));
+		System.out.println("Second Name validation: "+user.firstNameValidation(lname));
 		
 	}
 
